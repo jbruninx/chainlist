8f8d5fe7577ce2cc54b26c69f4c7ae25d407e71d
Johan Bruninx <jbruninx@gmail.com>
2018-01-05 22:55:53 +0100

    Addint frontend files to Github pages

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,5 +1,5 @@
-copy -r src/ docs/
-copy build/contracts/ChainList.json docs/
+cp -r src/ docs/
+cp build/contracts/ChainList.json docs/
 git add .
 git commit -m "Addint frontend files to Github pages"
 git push
